bd3d7bc983078eb1b9b9db61433fbed5179c5c74
Nathan <nathaniel.hermann.keeler@hs-furtwangen.de>
2021-10-14 15:55:54 +0200

    adds viewport update and laser movement

--- LaserLeague/Script/Source/Main.ts
+++ LaserLeague/Script/Source/Main.ts
@@ -5,15 +5,23 @@ namespace Script {
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
+  let transform: FudgeCore.Matrix4x4;
+
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+
+    let graph: FudgeCore.Node = viewport.getBranch();
+    let laser : FudgeCore.Node = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser")[0];
+    transform = laser.getComponent(FudgeCore.ComponentTransform).mtxLocal;
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.world.simulate();  // if physics is included and used
+    transform.rotateZ(1);
+
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
