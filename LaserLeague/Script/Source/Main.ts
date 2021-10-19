d7c9dd6675de8e9662ec8e4054cc0cccb5b59800
Nathan <nathaniel.hermann.keeler@hs-furtwangen.de>
2021-10-19 12:30:08 +0200

    redesigned internal.json + adds florin char to main

--- LaserLeague/Script/Source/Main.ts
+++ LaserLeague/Script/Source/Main.ts
@@ -1,11 +1,11 @@
 namespace Script {
   import ƒ = FudgeCore;
-  ƒ.Debug.info("Main Program Template running!")
+  ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
-  let transform: FudgeCore.Matrix4x4;
+  let transform: ƒ.Matrix4x4;
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
@@ -13,15 +13,14 @@ namespace Script {
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
 
-    let graph: FudgeCore.Node = viewport.getBranch();
-    let laser : FudgeCore.Node = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser")[0];
-    transform = laser.getComponent(FudgeCore.ComponentTransform).mtxLocal;
+    let graph: ƒ.Node = viewport.getBranch();
+    let laser: ƒ.Node = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser")[0];
+    transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.world.simulate();  // if physics is included and used
-    transform.rotateZ(1);
-
+    transform.rotateZ(5);
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
