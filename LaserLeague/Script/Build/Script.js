595db124cc78da29a57d25663c9926e787ba87c7
Nathan <nathaniel.hermann.keeler@hs-furtwangen.de>
2021-10-27 12:30:42 +0200

    adds copy of laser in form of graph

--- LaserLeague/Script/Build/Script.js
+++ LaserLeague/Script/Build/Script.js
@@ -35,34 +35,43 @@ var Script;
 var Script;
 (function (Script) {
     var ƒ = FudgeCore;
-    ƒ.Debug.info("Main Program Template running!");
-    let viewport;
-    document.addEventListener("interactiveViewportStarted", start);
+    ƒ.Debug.info("Welcome to LaserLeague!");
     //----- Variables -----
+    let fps = 60;
+    let viewport;
+    let transform;
     let agent;
     let laser;
-    let transform;
+    let laserCopy;
     let ctrVertical = new ƒ.Control("Forward", 1, 0 /* PROPORTIONAL */);
     ctrVertical.setDelay(100);
     let ctrRotation = new ƒ.Control("Rotation", 1, 0 /* PROPORTIONAL */);
     ctrRotation.setDelay(80);
-    function start(_event) {
+    document.addEventListener("interactiveViewportStarted", start);
+    async function start(_event) {
         viewport = _event.detail;
-        let graph = viewport.getBranch();
-        agent = graph.getChildrenByName("Agents")[0].getChildrenByName("Agent")[0];
-        laser = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser")[0];
+        // Load Graph and its Nodes for access
+        let root = viewport.getBranch();
+        agent = root.getChildrenByName("Agents")[0].getChildrenByName("Agent")[0]; // picks out the first single agent node
+        laser = root.getChildrenByName("Lasers")[0].getChildrenByName("Laser")[0]; // picks out the first single laser node
         transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
-        //Camera
+        // Camera
         viewport.camera.mtxPivot.translateZ(-15);
+        // Add another laser as graph
+        let graphLaser = await ƒ.Project.registerAsGraph(laser, false);
+        laserCopy = await ƒ.Project.createGraphInstance(graphLaser);
+        root.getChildrenByName("Lasers")[0].addChild(laserCopy);
+        // laserCopy.addComponent(new ƒ.ComponentTransform);
+        laserCopy.mtxLocal.translateX(5);
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, 120); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, fps); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         // ƒ.Physics.world.simulate();  // if physics is included and used
         let deltaTime = ƒ.Loop.timeFrameReal / 1000;
-        let speedLaserRotation = 60; // degrees per second
         let speedAgentTranslation = 5; // meters per second
         let speedAgentRotation = 400; // meters per second
+        let speedLaserRotation = 60; // degrees per second
         //----- Controlls -----
         let ctrVerticalValue = (ƒ.Keyboard.mapToValue(-1, 0, [ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN])
             + ƒ.Keyboard.mapToValue(1, 0, [ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP]));
@@ -73,7 +82,7 @@ var Script;
         ctrRotation.setInput(ctrRotationValue * deltaTime * speedAgentRotation);
         agent.mtxLocal.rotateZ(ctrRotation.getOutput());
         //------------------
-        //transform.rotateZ(speedLaserRotation * deltaTime);
+        transform.rotateZ(speedLaserRotation * deltaTime);
         viewport.draw();
         checkCollision();
         ƒ.AudioManager.default.update();
@@ -82,9 +91,8 @@ var Script;
         // Loop through all laser nodes and their beams
         // Check if posLocal x value are >1 or <-1
         // Alert User
-        let beam = laser.getChildren()[0];
-        let posLocal = ƒ.Vector3.TRANSFORMATION(agent.mtxWorld.translation, beam.mtxWorldInverse, true);
-        console.log(posLocal.toString());
+        // let posLocal: ƒ.Vector3 = ƒ.Vector3.TRANSFORMATION(agent.mtxWorld.translation, beams.mtxWorldInverse, true);
+        // console.log(posLocal.toString());
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- LaserLeague/Script/Source/Main.ts
+++ LaserLeague/Script/Source/Main.ts
@@ -1,41 +1,52 @@
 namespace Script {
   import ƒ = FudgeCore;
-  ƒ.Debug.info("Main Program Template running!");
-
-  let viewport: ƒ.Viewport;
-  document.addEventListener("interactiveViewportStarted", <EventListener>start);
-
+  ƒ.Debug.info("Welcome to LaserLeague!");
+  
   //----- Variables -----
+  let fps: number = 60;
+  let viewport: ƒ.Viewport;
+  let transform: ƒ.Matrix4x4;
   let agent: ƒ.Node;
   let laser: ƒ.Node;
-  let transform: ƒ.Matrix4x4;
+  let laserCopy: ƒ.GraphInstance;
+
   let ctrVertical: ƒ.Control = new ƒ.Control("Forward", 1, ƒ.CONTROL_TYPE.PROPORTIONAL);
   ctrVertical.setDelay(100);
   let ctrRotation: ƒ.Control = new ƒ.Control("Rotation", 1, ƒ.CONTROL_TYPE.PROPORTIONAL);
   ctrRotation.setDelay(80);
+  
+  document.addEventListener("interactiveViewportStarted", <EventListener><unknown>start);
 
-  function start(_event: CustomEvent): void {
+  async function start(_event: CustomEvent): Promise<void> {
     viewport = _event.detail;
-
-    let graph: ƒ.Node = viewport.getBranch();
-    agent = graph.getChildrenByName("Agents")[0].getChildrenByName("Agent")[0];
-    laser = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser")[0];
+    
+    // Load Graph and its Nodes for access
+    let root: ƒ.Node = viewport.getBranch();
+    agent = root.getChildrenByName("Agents")[0].getChildrenByName("Agent")[0]; // picks out the first single agent node
+    laser = root.getChildrenByName("Lasers")[0].getChildrenByName("Laser")[0]; // picks out the first single laser node
     transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
-
-    //Camera
+    
+    // Camera
     viewport.camera.mtxPivot.translateZ(-15);
-
+    
+    // Add another laser as graph
+    let graphLaser: ƒ.Graph = await ƒ.Project.registerAsGraph(laser, false);
+    laserCopy = await ƒ.Project.createGraphInstance(graphLaser);
+    root.getChildrenByName("Lasers")[0].addChild(laserCopy);
+    // laserCopy.addComponent(new ƒ.ComponentTransform);
+    laserCopy.mtxLocal.translateX(5);
+    
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, 120);  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, fps);  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.world.simulate();  // if physics is included and used
 
     let deltaTime: number =ƒ.Loop.timeFrameReal / 1000;
-    let speedLaserRotation: number = 60; // degrees per second
     let speedAgentTranslation: number = 5; // meters per second
     let speedAgentRotation: number = 400; // meters per second
+    let speedLaserRotation: number = 60; // degrees per second
     
     //----- Controlls -----
     let ctrVerticalValue: number = (
@@ -53,12 +64,11 @@ namespace Script {
     agent.mtxLocal.rotateZ(ctrRotation.getOutput());
     //------------------
 
-    //transform.rotateZ(speedLaserRotation * deltaTime);
+    transform.rotateZ(speedLaserRotation * deltaTime);
 
     viewport.draw();
 
     checkCollision();
-
 
     ƒ.AudioManager.default.update();
   }
@@ -67,8 +77,8 @@ namespace Script {
     // Loop through all laser nodes and their beams
     // Check if posLocal x value are >1 or <-1
     // Alert User
-    let beam: ƒ.Node = laser.getChildren()[0];
-    let posLocal: ƒ.Vector3 = ƒ.Vector3.TRANSFORMATION(agent.mtxWorld.translation, beam.mtxWorldInverse, true);
-    console.log(posLocal.toString());
+
+    // let posLocal: ƒ.Vector3 = ƒ.Vector3.TRANSFORMATION(agent.mtxWorld.translation, beams.mtxWorldInverse, true);
+    // console.log(posLocal.toString());
   }
 }
