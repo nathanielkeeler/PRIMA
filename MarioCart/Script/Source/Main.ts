34b05bcc834182844c91cdbd7119ac0ba9d8bf4a
Nathan <nathaniel.hermann.keeler@hs-furtwangen.de>
2021-12-07 12:16:06 +0100

    physics wip

--- MarioCart/Script/Source/Main.ts
+++ MarioCart/Script/Source/Main.ts
@@ -4,7 +4,9 @@ namespace Script {
 
   let graph: ƒ.Graph;
   let viewport: ƒ.Viewport;
+
   let cart: ƒ.Node;
+  let cartBody: ƒ.ComponentRigidbody;
 
   let cmpMeshTerrain: ƒ.ComponentMesh;
   let mtxTerrain: ƒ.Matrix4x4;
@@ -13,8 +15,8 @@ namespace Script {
   let camera: ƒ.Node = new ƒ.Node("cameraNode");
   let cmpCamera = new ƒ.ComponentCamera();
 
-  let ctrForward: ƒ.Control = new ƒ.Control("Forward", 15, ƒ.CONTROL_TYPE.PROPORTIONAL);
-  ctrForward.setDelay(700);
+  let ctrForward: ƒ.Control = new ƒ.Control("Forward", 200, ƒ.CONTROL_TYPE.PROPORTIONAL);
+  ctrForward.setDelay(200);
   let ctrTurn: ƒ.Control = new ƒ.Control("Turn", 100, ƒ.CONTROL_TYPE.PROPORTIONAL);
   ctrTurn.setDelay(200);
 
@@ -31,6 +33,8 @@ namespace Script {
     mtxTerrain = cmpMeshTerrain.mtxWorld;
 
     cart = graph.getChildrenByName("Cart")[0];
+    cartBody = cart.getComponent(ƒ.ComponentRigidbody);
+
 
     cameraTrackCart();
 
@@ -39,25 +43,38 @@ namespace Script {
   }
 
   function update(_event: Event): void {
-    // ƒ.Physics.world.simulate();  // if physics is included and used
+    let forceNodes: ƒ.Node[] = cart.getChildrenByName("Forces")[0].getChildren();
+    let force: ƒ.Vector3 = ƒ.Vector3.SCALE(ƒ.Physics.world.getGravity(), -cartBody.mass / forceNodes.length);
+
+    for (let forceNode of forceNodes) {
+      let posForce: ƒ.Vector3 = forceNode.getComponent(ƒ.ComponentMesh).mtxWorld.translation;
+      let terrainInfo: ƒ.TerrainInfo = meshTerrain.getTerrainInfo(posForce, mtxTerrain);
+      let height: number = posForce.y - terrainInfo.position.y;
+      // console.log(height);
+      cartBody.applyForceAtPoint(force, posForce);
+    }
+
+    ƒ.Physics.world.simulate();  // if physics is included and used
 
     camera.mtxLocal.translation = cart.mtxWorld.translation;
     camera.mtxLocal.rotation = new ƒ.Vector3(0, cart.mtxWorld.rotation.y, 0);
 
-    let speedCartRotation: number = 1.8;
-    let deltaTime: number = ƒ.Loop.timeFrameReal / 1000;
+    // let speedCartRotation: number = 1.8;
+    // let deltaTime: number = ƒ.Loop.timeFrameReal / 1000;
 
     let forward: number = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP], [ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]);
-    ctrForward.setInput(forward * deltaTime);
-    cart.mtxLocal.translateZ(ctrForward.getOutput());
+    ctrForward.setInput(forward);
+    cartBody.applyForce(ƒ.Vector3.SCALE(cartBody.node.mtxLocal.getZ(), ctrForward.getOutput()));
+    // cart.mtxLocal.translateZ(ctrForward.getOutput() * deltaTime);
 
     let turn: number = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT], [ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]);
-    ctrTurn.setInput(turn * deltaTime * speedCartRotation);
-    cart.mtxLocal.rotateY(ctrTurn.getOutput());
+    ctrTurn.setInput(turn);
+    cartBody.applyTorque(ƒ.Vector3.SCALE(ƒ.Vector3.Y(), ctrTurn.getOutput()));
+    // cart.mtxLocal.rotateY(ctrTurn.getOutput() * deltaTime * speedCartRotation);
 
-    let terrainInfo: ƒ.TerrainInfo = meshTerrain.getTerrainInfo(cart.mtxLocal.translation, mtxTerrain);
-    cart.mtxLocal.translation = terrainInfo.position;
-    cart.mtxLocal.showTo(ƒ.Vector3.SUM(terrainInfo.position, cart.mtxLocal.getZ()), terrainInfo.normal);
+    // let terrainInfo: ƒ.TerrainInfo = meshTerrain.getTerrainInfo(cart.mtxLocal.translation, mtxTerrain);
+    // cart.mtxLocal.translation = terrainInfo.position;
+    // cart.mtxLocal.showTo(ƒ.Vector3.SUM(terrainInfo.position, cart.mtxLocal.getZ()), terrainInfo.normal);
 
     viewport.draw();
     ƒ.AudioManager.default.update();
