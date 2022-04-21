c0228018b401f2c4809eeddefe88c66e538bb116
Nathan <nathaniel.hermann.keeler@hs-furtwangen.de>
2022-04-21 15:24:58 +0200

    adds player + camera controls

--- Slenderman/Script/Build/Script.js
+++ Slenderman/Script/Build/Script.js
@@ -40,16 +40,43 @@ var Slenderman;
 (function (Slenderman) {
     var ƒ = FudgeCore;
     let viewport;
+    let root;
+    let player;
+    let playerCmpCam;
+    let speedRot = 0.1;
+    let rotationX = 0;
+    let ctrWalk = new ƒ.Control("ctrWalk", 1.5, 0 /* PROPORTIONAL */);
+    // ctrWalk.setDelay(500);
     document.addEventListener("interactiveViewportStarted", start);
     function start(_event) {
         viewport = _event.detail;
+        root = viewport.getBranch();
+        player = root.getChildrenByName("Player")[0];
+        playerCmpCam = root.getChildrenByName("Player")[0].getChildrenByName("Camera")[0].getComponent(ƒ.ComponentCamera);
+        viewport.camera = playerCmpCam; //Active viewport camera is player view
+        viewport.getCanvas().addEventListener("pointermove", hndPointerMove);
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start();
     }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
+        controlWalk();
         viewport.draw();
         ƒ.AudioManager.default.update();
+    }
+    function hndPointerMove(_event) {
+        player.mtxLocal.rotateY(-_event.movementX * speedRot);
+        rotationX += _event.movementY * speedRot;
+        rotationX = Math.min(60, Math.max(-60, rotationX));
+        playerCmpCam.mtxPivot.rotation = ƒ.Vector3.X(rotationX);
+    }
+    function controlWalk() {
+        let inputForward = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP], [ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]);
+        ctrWalk.setInput(inputForward);
+        player.mtxLocal.translateZ(ctrWalk.getOutput() * ƒ.Loop.timeFrameGame / 1000);
+        let inputSideways = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_RIGHT], [ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_LEFT]);
+        ctrWalk.setInput(inputSideways);
+        player.mtxLocal.translateX(ctrWalk.getOutput() * ƒ.Loop.timeFrameGame / 1000);
     }
 })(Slenderman || (Slenderman = {}));
 //# sourceMappingURL=Script.js.map
--- Slenderman/Script/Source/Main.ts
+++ Slenderman/Script/Source/Main.ts
@@ -2,12 +2,29 @@ namespace Slenderman {
   import ƒ = FudgeCore;
 
   let viewport: ƒ.Viewport;
+  let root: ƒ.Node;
+  let player: ƒ.Node;
+  let playerCmpCam: ƒ.ComponentCamera;
+
+  let speedRot: number = 0.1;
+  let rotationX: number = 0;
+
+  let ctrWalk: ƒ.Control = new ƒ.Control("ctrWalk", 1.5, ƒ.CONTROL_TYPE.PROPORTIONAL);
+  // ctrWalk.setDelay(500);
+
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
 
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+
+    root = viewport.getBranch();
+    player = root.getChildrenByName("Player")[0];
+    playerCmpCam = root.getChildrenByName("Player")[0].getChildrenByName("Camera")[0].getComponent(ƒ.ComponentCamera);
+    viewport.camera = playerCmpCam; //Active viewport camera is player view
+
+    viewport.getCanvas().addEventListener("pointermove", hndPointerMove);
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();
@@ -17,7 +34,29 @@ namespace Slenderman {
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
+
+    controlWalk();
+
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
+
+
+  function hndPointerMove(_event: PointerEvent): void {
+    player.mtxLocal.rotateY(-_event.movementX * speedRot);
+    rotationX += _event.movementY * speedRot;
+    rotationX = Math.min(60, Math.max(-60, rotationX));
+    playerCmpCam.mtxPivot.rotation = ƒ.Vector3.X(rotationX);
+  }
+
+  function controlWalk(): void {
+    let inputForward: number = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP], [ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]);
+    ctrWalk.setInput(inputForward);
+    player.mtxLocal.translateZ(ctrWalk.getOutput() * ƒ.Loop.timeFrameGame / 1000);
+    
+    let inputSideways: number = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_RIGHT], [ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_LEFT]);
+    ctrWalk.setInput(inputSideways);
+    player.mtxLocal.translateX(ctrWalk.getOutput() * ƒ.Loop.timeFrameGame / 1000);
+  }
 }
+
