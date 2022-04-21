841d387dfa7702deeeeb79b2f4a580a3bbdd0254
Nathan <nathaniel.hermann.keeler@hs-furtwangen.de>
2022-04-21 12:05:07 +0200

    update

--- Slenderman/Script/Source/Main.ts
+++ Slenderman/Script/Source/Main.ts
@@ -1,16 +1,19 @@
 namespace Script {
   import ƒ = FudgeCore;
-  ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
+
+
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();
   }
+
+
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
