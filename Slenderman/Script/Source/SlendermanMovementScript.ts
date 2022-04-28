f3ee43db93ea3a3d72926637766abcb209f77bdd
Nathan <nathaniel.hermann.keeler@hs-furtwangen.de>
2022-04-28 15:50:51 +0200

    adds slenderman movement by script

---
+++ Slenderman/Script/Source/SlendermanMovementScript.ts
@@ -0,0 +1,57 @@
+namespace Script {
+    import ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script);  // Register the namespace to FUDGE for serialization
+
+    export class SlendermanMovementScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        public static readonly iSubclass: number = ƒ.Component.registerSubclass(SlendermanMovementScript);
+        // Properties may be mutated by users in the editor via the automatically created user interface
+        private timeToChange: number = 0;
+        private direction: ƒ.Vector3 = ƒ.Vector3.ZERO();
+
+
+        constructor() {
+            super();
+
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+
+            // Listen to this component being added to or removed from a node
+            this.addEventListener(ƒ.EVENT.COMPONENT_ADD, this.hndEvent);
+            this.addEventListener(ƒ.EVENT.COMPONENT_REMOVE, this.hndEvent);
+            this.addEventListener(ƒ.EVENT.NODE_DESERIALIZED, this.hndEvent);
+        }
+
+        // Activate the functions of this component as response to events
+        public hndEvent = (_event: Event): void => {
+            switch (_event.type) {
+                case ƒ.EVENT.COMPONENT_ADD:
+                    this.node.addEventListener(ƒ.EVENT.RENDER_PREPARE, this.move);
+                    break;
+                case ƒ.EVENT.COMPONENT_REMOVE:
+                    this.removeEventListener(ƒ.EVENT.COMPONENT_ADD, this.hndEvent);
+                    this.removeEventListener(ƒ.EVENT.COMPONENT_REMOVE, this.hndEvent);
+                    break;
+                case ƒ.EVENT.NODE_DESERIALIZED:
+                    // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                    break;
+            }
+        }
+
+        private move = (_event: Event): void => {
+            this.node.mtxLocal.translate(ƒ.Vector3.SCALE(this.direction, ƒ.Loop.timeFrameGame / 1000));
+            
+            if (this.timeToChange > ƒ.Time.game.get())
+                return;
+
+            this.timeToChange = ƒ.Time.game.get() + 3000;
+            this.direction = ƒ.Random.default.getVector3(new ƒ.Vector3(-1, 0, -1), new ƒ.Vector3(1, 0, 1));
+        }
+
+        // protected reduceMutator(_mutator: ƒ.Mutator): void {
+        //   // delete properties that should not be mutated
+        //   // undefined properties and private fields (#) will not be included by default
+        // }
+    }
+}
